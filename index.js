558630f6abed83d28bcd53fc519474c69f619690
natanael <rojonatanael99@gmail.com>
2024-08-27 22:23:34 -0400

    Default value in object

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function urlValidator(url) {
 
 // POST /api/shorturl
 app.post('/api/shorturl', (req, res) => {
-  const { url } = req.body;
+  const { url = '' } = req.body;
 
   // Validate the URL
   if (!urlValidator(url)) {
